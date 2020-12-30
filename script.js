f8cd588c082732e0511eff3bfb6b4b1e694b514d
cybercleng <57181717+cybercleng@users.noreply.github.com>
2020-12-30 21:39:28 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function stonesUnturned() {
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLScBTtGplxDjqSc_SK6GHKiuD6-vgPKTnOw0GWhIoKKFT6cHgg/viewform");
+  }
+
+function chaosRecalibration() {
+    window.open("https://docs.google.com/forms/d/1urdAeNEM6F34TSfLiOlqMdSYqKNL66jBix3MvmwLUTI/edit?usp=forms_home&ths=true");
+  }
+
+function rescuingYourself() {
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSfrAcdQHUsXVa9O1TS10THOZNWNx2ukYR9OYFDYkoonc0yWwg/viewform");
+  }
+
+function chaosRecalibration() {
+    window.open("https://docs.google.com/forms/d/1urdAeNEM6F34TSfLiOlqMdSYqKNL66jBix3MvmwLUTI/edit?usp=forms_home&ths=true");
+  }
+
+  function chaosRecalibration() {
+    window.open("https://docs.google.com/forms/d/1urdAeNEM6F34TSfLiOlqMdSYqKNL66jBix3MvmwLUTI/edit?usp=forms_home&ths=true");
+  }
+
+  function allForms() {
+    window.open("https://docs.google.com/forms/u/0/");
+  }
